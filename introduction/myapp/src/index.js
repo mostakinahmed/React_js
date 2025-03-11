aba9f0304231a9de4494f28d649499d30cf86260
mostakinahmed <mostakin4556@gmail.com>
2025-03-11 21:11:44 +0600

    a

--- introduction/myapp/src/index.js
+++ introduction/myapp/src/index.js
@@ -2,4 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>Hello, world!</h1>);
+const todoTitle = "Learn React with Mostakin ahmed";
+const date = new Date();
+const year = date.getFullYear();
+
+root.render(
+  <div>
+    <h1>Hello, {todoTitle} Mostakin ahmed !</h1>
+    <h2>Hello, ahmed !</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam,</p>
+    <h1>{year}</h1>
+  </div>
+);
